fb8b228f0cd657e137624f3b41e3c2827e6552b8
Kristian Tagesen <tagesen.kristian@gmail.com>
2023-12-13 13:34:42 +0100

    broken up lunarvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,8 +1,5 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.colorscheme = "dracula"
+require('settings')
+require('keymaps')
 
 lvim.plugins = {
   { "maxmx03/dracula.nvim" },
@@ -10,9 +7,6 @@ lvim.plugins = {
 vim.opt.linebreak = true
 vim.opt.relativenumber = true
 vim.opt.lazyredraw = true
-lvim.leader = ","
-lvim.builtin.lualine.options.theme = "dracula"
-lvim.builtin.lualine.sections.lualine_a = { "mode" }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "flake8", filetypes = { "python" } } }
@@ -20,19 +14,6 @@ linters.setup { { command = "flake8", filetypes = { "python" } } }
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup{{ name="black"}, }
 
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "json",
-  "yaml",
-  "lua",
-  "python",
-}
-
 lvim.keys.normal_mode["0"] = "^" -- Remap VIM 0 to first non-blank characte
-lvim.keys.normal_mode["<leader><cr>"] = ":noh<cr>" -- Remap VIM 0 to first non-blank characte
-lvim.keys.normal_mode["<space>"] = "/" -- Remap VIM 0 to first non-blank characte
--- Set default syntax for all files
--- vim.api.nvim_create_autocmd({"BufNewFile" ,"BufRead"}, {
---   pattern = {"*"},
---   command = "if &ft == '' | set ft=zsh | endif",
--- })
+lvim.keys.normal_mode["<leader><cr>"] = ":noh<cr>" -- Remap <leader>+Enter to clear search highlight
+lvim.keys.normal_mode["<space>"] = "/" -- Remap space to search
---
+++ config/lvim/lua/settings.lua
@@ -0,0 +1,14 @@
+lvim.colorscheme = "dracula"
+
+lvim.leader = ","
+lvim.line_wrap_cursor_movement = false
+lvim.builtin.lualine.options.theme = "dracula"
+lvim.builtin.lualine.sections.lualine_a = { "mode" }
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "json",
+  "yaml",
+  "lua",
+  "python",
+}
