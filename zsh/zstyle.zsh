aabd6341e7e9181eafe0396c50b6e6d401f71ac7
kritag <tagesen.kristian@gmail.com>
2024-01-26 17:47:46 +0100

    zstyle

--- zsh/zstyle.zsh
+++ zsh/zstyle.zsh
@@ -1,5 +1,5 @@
 zstyle -e ':completion:*' hosts 'reply=($(< ~/.ssh/ssh_hosts))'
-zstyle ':completion:*:*:*:*:*' menu select
+#zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:docker:*' option-stacking yes
 zstyle ':completion:*:*:docker-*:*' option-stacking yes
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
@@ -10,15 +10,80 @@ zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
 zstyle ':completion:*:functions' ignore-patterns '_*'
 # if you end up using a directory as argument
 zstyle ':completion:*' squeeze-slashes true
-# Group matches and describe.
-zstyle ':completion:*:matches' group 'yes'
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:corrections' format ' %F{red}-- %d (errors: %e) --%f'
-zstyle ':completion:*:descriptions' format ' %F{purple}-- %d --%f'
-zstyle ':completion:*:messages' format ' %F{green} -- %d --%f'
-zstyle ':completion:*:warnings' format ' %F{yellow}-- no matches found --%f'
-zstyle ':completion:*' format ' %F{blue}-- %d --%f'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' verbose yes
-zstyle ':fzf-tab:*' fzf-min-height '1000'
+zstyle ':completion:*:descriptions' format %d
+zstyle ':fzf-tab:complete:*' extra-opts --preview-window=:hidden
+zstyle ':fzf-tab:*' show-group full
+#zstyle ':fzf-tab:*' fzf-min-height '1000'
+zstyle ':fzf-tab:complete:*:options' fzf-preview 
+zstyle ':fzf-tab:complete:*:argument-1' fzf-preview
+# --complete
+#zstyle ':fzf-tab:complete:*' fzf-preview 'less ${realpath#--*=}'
+# --user-expand
+zstyle ':fzf-tab:user-expand::' fzf-preview 'less $word'
+# -parameter
+zstyle ':fzf-tab:complete:((-parameter-|unset):|(export|typeset|declare|local):argument-rest)' fzf-preview 'echo ${(P)word}'
+# -equal
+zstyle ':fzf-tab:complete:(-equal-:|(\\|*/|)(sudo|proxychains|strace):argument-1|pudb:option--pre-run-1)' fzf-preview 'less =$word'
+# -command-
+zstyle ':fzf-tab:complete:(-command-:|command:option-(v|V)-rest)' fzf-preview 'case $group in
+'external command')
+  less =$word
+  ;;
+'executable file')
+  less ${realpath#--*=}
+  ;;
+'builtin command')
+  run-help $word | bat -lman
+  ;;
+parameter)
+  echo ${(P)word}
+  ;;
+esac'
+# Systemctl
+zstyle ':fzf-tab:complete:systemctl-(status|(re|)start|(dis|en)able):*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+# Man
+zstyle ':fzf-tab:complete:(\\|*/|)man:' fzf-preview 'man $word | bat -lman'
+zstyle ':fzf-tab:complete:(\\|*/|)bat:argument-rest' fzf-preview 'case $group in
+subcommand)
+  bat cache --help | bat -lhelp
+  ;;
+*)
+  [[ -f ${realpath#--*=} ]] && bat ${realpath#--*=} || less ${realpath#--*=}
+  ;;
+esac'
+# Keymap
+zstyle ':fzf-tab:complete:(\\|)bindkey:option-M-1' fzf-preview 'bindkey -M$word | bat -ltsv'
+# gh
+zstyle ':fzf-tab:complete:gh:' fzf-preview 'gh help $word | bat -lhelp'
+# git
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
+zstyle ':fzf-tab:complete:git-log:*' fzf-preview	'git log --color=always $word'
+zstyle ':fzf-tab:complete:git-help:*' fzf-preview	'git help $word | bat -plman --color=always'
+zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+	'case "$group" in
+	"commit tag") git show --color=always $word ;;
+	*) git show --color=always $word | delta ;;
+	esac'
+zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+	'case "$group" in
+	"modified file") git diff $word | delta ;;
+	"recent commit object name") git show --color=always $word | delta ;;
+	*) git log --color=always $word ;;
+	esac'
+# jq
+zstyle ':fzf-tab:complete:(\\|*/|)jq:argument-rest' fzf-preview 'jq -Cr . $realpath 2>/dev/null || less $realpath'
+# pkill
+zstyle ':fzf-tab:complete:(\\|*/|)(pkill:o-argument-rest|killall:argument-1)' fzf-preview 'ps -C$word -wocmd --no-headers | bat -lsh'
+# ps
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview 'ps -p$word -wocmd --no-headers | bat -lsh'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
+# read
+zstyle ':fzf-tab:complete:(\\|)read:argument-1' fzf-preview 'echo ${(P)word}'
+
+_FZF_TAB_FZF_FLAGS=(
+    '--height=80%' \
+    '--layout=reverse' \
+)
+zstyle ':fzf-tab:complete:*' fzf-preview '([[ -f $realpath ]] && bat --color=always "$realpath") || ([[ -d $realpath ]] && (eza --long --git --group-directories-first --icons --color=always "$realpath"))'
+zstyle ':fzf-tab:complete:*' fzf-flags '--preview-window=50%' $_FZF_TAB_FZF_FLAGS
