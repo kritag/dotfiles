fae51ba0b103725bba4bbc3ff757bb36173ea460
Kristian Tagesen <tagesen.kristian@gmail.com>
2024-03-22 11:55:23 +0100

    asd

--- scripts/pwquality.sh
+++ scripts/pwquality.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-/usr/bin/mkdir /run/intune/0
-/usr/bin/mkdir /run/intune/1000
+/usr/bin/mkdir -p /run/intune/0
+/usr/bin/mkdir -p /run/intune/1000
 /usr/bin/touch /run/intune/0/pwquality
 /usr/bin/touch /run/intune/1000/pwquality
 echo "password requisite pam_pwquality.so minlen=12 dcredit=-4 lcredit=-7 ocredit=0 ucredit=-1" | tee > /run/intune/0/pwquality /run/intune/1000/pwquality
