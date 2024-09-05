f9139c9a2c4bd895237cc6208d6aede913dce2f7
Kristian Tagesen <tagesen.kristian@gmail.com>
2024-09-05 08:52:15 +0200

    removed flatpak

--- scripts/set_catppuccin_theme.sh
+++ scripts/set_catppuccin_theme.sh
@@ -7,9 +7,6 @@ gsettings set org.gnome.desktop.interface cursor-theme "Catppuccin-Mocha-Mauve-C
 gsettings set org.gnome.desktop.interface cursor-size 32
 # Set theme for GTK 4.0
 mkdir -p "${HOME}/.config/gtk-4.0"
-ln -sf /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/gtk-4.0/assets "${HOME}/.config/gtk-4.0/assets"
-ln -sf /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/gtk-4.0/gtk.css "${HOME}/.config/gtk-4.0/gtk.css"
-ln -sf /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/gtk-4.0/gtk-dark.css "${HOME}/.config/gtk-4.0/gtk-dark.css"
-# Make theme available for flatpaks
-cp -r /usr/share/icons/Catppuccin-Mocha-Mauve-Cursors/ "${HOME}/.icons/Catppuccin-Mocha-Mauve-Cursors"
-cp -r /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/ "${HOME}/.themes/Catppuccin-Mocha-Standard-Mauve-Dark"
+ln -sf /usr/share/themes/catppuccin-mocha-mauve-standard+default/gtk-4.0/assets "${HOME}/.config/gtk-4.0/assets"
+ln -sf /usr/share/themes/catppuccin-mocha-mauve-standard+default/gtk-4.0/gtk.css "${HOME}/.config/gtk-4.0/gtk.css"
+ln -sf /usr/share/themes/catppuccin-mocha-mauve-standard+default/gtk-4.0/gtk-dark.css "${HOME}/.config/gtk-4.0/gtk-dark.css"
