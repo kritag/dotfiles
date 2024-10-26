c9f07fd302f3d37fb7b7b3309f392d9b34f1afd0
Kristian Tagesen <tagesen.kristian@gmail.com>
2024-10-26 12:40:30 +0200

    asd

--- config/hypr/scripts/slack.sh
+++ config/hypr/scripts/slack.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-/usr/bin/slack &
+/usr/bin/slack -s --enable-features=UseOzonePlatform --ozone-platform=wayland --force-device-scale-factor %U &
 while ! hyprctl clients | grep -q "Slack"; do
   sleep 0.2
 done
