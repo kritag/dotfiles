695d2aa0dfe0374962c15ea8eaa0382e18d56f79
Kristian Tagesen <tagesen.kristian@gmail.com>
2024-11-15 12:14:26 +0100

    asd

--- zsh/zshkeybindings.zsh
+++ zsh/zshkeybindings.zsh
@@ -10,40 +10,44 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   zle -N zle-line-init
   zle -N zle-line-finish
 fi
-# Use emacs key bindings
-bindkey -e
+# # Use emacs key bindings
+# bindkey -e
 # [Ctrl-RightArrow] - move forward one word
-bindkey -M emacs '^[[1;5C' forward-word
-bindkey -M viins '^[[1;5C' forward-word
-bindkey -M vicmd '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
-bindkey -M emacs '^[[1;5D' backward-word
-bindkey -M viins '^[[1;5D' backward-word
-bindkey -M vicmd '^[[1;5D' backward-word
-# [Home] - Go to beginning of line
-if [[ -n "${terminfo[khome]}" ]]; then
-  bindkey -M emacs "${terminfo[khome]}" beginning-of-line
-  bindkey -M viins "${terminfo[khome]}" beginning-of-line
-  bindkey -M vicmd "${terminfo[khome]}" beginning-of-line
-fi
-# [End] - Go to end of line
-if [[ -n "${terminfo[kend]}" ]]; then
-  bindkey -M emacs "${terminfo[kend]}"  end-of-line
-  bindkey -M viins "${terminfo[kend]}"  end-of-line
-  bindkey -M vicmd "${terminfo[kend]}"  end-of-line
-fi
-# [Shift-Tab] - move through the completion menu backwards
-if [[ -n "${terminfo[kcbt]}" ]]; then
-  bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
-  bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
-  bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
-fi
-# [Delete] - delete forward
-bindkey -M emacs "^[[3~" delete-char
-bindkey -M viins "^[[3~" delete-char
-bindkey -M vicmd "^[[3~" delete-char
+# bindkey -M emacs '^[[1;5C' forward-word
+# bindkey -M viins '^[[1;5C' forward-word
+# bindkey -M vicmd '^[[1;5C' forward-word
+# # [Ctrl-LeftArrow] - move backward one word
+# bindkey -M emacs '^[[1;5D' backward-word
+# bindkey -M viins '^[[1;5D' backward-word
+# bindkey -M vicmd '^[[1;5D' backward-word
+# # [Home] - Go to beginning of line
+# if [[ -n "${terminfo[khome]}" ]]; then
+#   bindkey -M emacs "${terminfo[khome]}" beginning-of-line
+#   bindkey -M viins "${terminfo[khome]}" beginning-of-line
+#   bindkey -M vicmd "${terminfo[khome]}" beginning-of-line
+# fi
+# # [End] - Go to end of line
+# if [[ -n "${terminfo[kend]}" ]]; then
+#   bindkey -M emacs "${terminfo[kend]}"  end-of-line
+#   bindkey -M viins "${terminfo[kend]}"  end-of-line
+#   bindkey -M vicmd "${terminfo[kend]}"  end-of-line
+# fi
+# # [Shift-Tab] - move through the completion menu backwards
+# if [[ -n "${terminfo[kcbt]}" ]]; then
+#   bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
+#   bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
+#   bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
+# fi
+# # [Delete] - delete forward
+# bindkey -M emacs "^[[3~" delete-char
+# bindkey -M viins "^[[3~" delete-char
+# bindkey -M vicmd "^[[3~" delete-char
 # Edit the current command line in $EDITOR
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line
-bindkey '^ ' autosuggest-accept
+# Autosuggest accept suggestion
+bindkey -M emacs '^ ' autosuggest-accept
+bindkey -M viins '^ ' autosuggest-accept
+bindkey -M vicmd '^ ' autosuggest-accept
+# Make the zsh-vi plugin not overwrite fzf-history-widget in insert mode
+function zvm_after_init() {
+  zvm_bindkey viins "^R" fzf-history-widget
+}
