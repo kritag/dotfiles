49ae668e325b00a35702b482557d5dd844f41604
Kristian Tagesen <tagesen.kristian@gmail.com>
2025-04-23 11:47:59 +0200

    Removed debug statement for terminal color in everforest

--- config/lazyvim/init.lua
+++ config/lazyvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.schedule(function()
-  vim.notify("Current terminal_color_0: " .. tostring(vim.g.terminal_color_0))
-end)
