d4f96fe30a6ccf7370e1cfbfaa27c84061926067
Kristian Tagesen <tagesen.kristian@gmail.com>
2025-07-03 15:11:10 +0200

    separate hypridle configs for laptop and desktop, and neovim plugin

--- config/lazyvim/lua/config/autocmds.lua
+++ config/lazyvim/lua/config/autocmds.lua
@@ -21,25 +21,25 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "yaml",
-  callback = function(args)
-    local bufnr = args.buf
-    local clients = vim.lsp.get_clients({ name = "yamlls", bufnr = bufnr })
-
-    if #clients > 0 then
-      require("config.yaml_k8s_crds").init(bufnr)
-    else
-      vim.api.nvim_create_autocmd("LspAttach", {
-        once = true,
-        buffer = bufnr,
-        callback = function(lsp_args)
-          local client = vim.lsp.get_client_by_id(lsp_args.data.client_id)
-          if client and client.name == "yamlls" then
-            require("config.yaml_k8s_crds").init(bufnr)
-          end
-        end,
-      })
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "yaml",
+--   callback = function(args)
+--     local bufnr = args.buf
+--     local clients = vim.lsp.get_clients({ name = "yamlls", bufnr = bufnr })
+--
+--     if #clients > 0 then
+--       require("config.yaml_k8s_crds").init(bufnr)
+--     else
+--       vim.api.nvim_create_autocmd("LspAttach", {
+--         once = true,
+--         buffer = bufnr,
+--         callback = function(lsp_args)
+--           local client = vim.lsp.get_client_by_id(lsp_args.data.client_id)
+--           if client and client.name == "yamlls" then
+--             require("config.yaml_k8s_crds").init(bufnr)
+--           end
+--         end,
+--       })
+--     end
+--   end,
+-- })
