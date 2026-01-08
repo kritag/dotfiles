f7a2236348938b3f74a3045b2780dbe8a89a3ad5
kritag <3776749+kritag@users.noreply.github.com>
2026-01-08 20:30:46 +0100

    lots of fixes

--- config/spicetify/Themes/Pywal/update-colors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.config/spicetify/Themes/Pywal/
-cp -r $HOME/.cache/wal/colors-spicetify.ini $HOME/.config/spicetify/Themes/Pywal
-cd $HOME/.config/spicetify/Themes/Pywal
-mv colors-spicetify.ini color.ini
