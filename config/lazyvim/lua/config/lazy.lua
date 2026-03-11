674407c9beb0fcdfb9a7982c8ee2066ac82750be
kritag <3776749+kritag@users.noreply.github.com>
2026-03-11 12:43:36 +0100

    asd

--- config/lazyvim/lua/config/lazy.lua
+++ config/lazyvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.dot" },
