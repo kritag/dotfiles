8fa5082f5e5719af36f56215aeb25a15327efcf4
kritag <3776749+kritag@users.noreply.github.com>
2026-05-21 09:45:15 +0200

    new hyprland layout, small fix

--- config/hypr/general.lua
+++ config/hypr/general.lua
@@ -6,7 +6,7 @@ hl.config({
 		border_size = 2,
 		gaps_in = 5,
 		gaps_out = 10,
-		layout = "lua:grid",
+		layout = "dwindle",
 		resize_on_border = false,
 		col = {
 			active_border = { colors = { colors.base0F, colors.base0C }, angle = 45 },
