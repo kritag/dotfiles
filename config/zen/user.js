d7de5d12354ff26793ace573b2d755276294dde8
kritag <3776749+kritag@users.noreply.github.com>
2026-09-01 15:42:58 +0200

    add zen browser fix for top black bar

--- config/zen/user.js
+++ config/zen/user.js
@@ -9,4 +9,5 @@ user_pref("zen.theme.content-element-separation", 0);
 user_pref("zen.urlbar.behavior", "normal");
 user_pref("zen.view.compact.enable-at-startup", true);
 user_pref("zen.view.compact.toolbar-flash-popup", true);
+user_pref("zen.view.experimental-no-window-controls", true);
 user_pref("zen.view.show-newtab-button-top", false);
